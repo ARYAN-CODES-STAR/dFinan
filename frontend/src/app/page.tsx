23908fba40028fcde7d0353749276300228c3073
divyesh123-jain <divyeshjain318@gmail.com>
2024-10-22 21:27:55 +0530

    added table

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { TableFilter } from "@/components/Dashboard/Table";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Your crypto finance tracker
+      <TableFilter />
     </div>
   );
 }
